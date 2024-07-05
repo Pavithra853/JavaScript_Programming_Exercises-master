f00b4dd27c5e40543b4209ddfaa32acd341f44e0
pavithrall <pavithraradhai87@gmail.com>
2024-07-05 14:18:01 +0530

    ch2_pgm_07.js completed

--- 02_JS_variables/Ch2_pgm_07.js
+++ 02_JS_variables/Ch2_pgm_07.js
@@ -2,6 +2,10 @@
 
 var playerName = "Kandra";
 var locationName = "The Dungeon of Doom";
+var health=50;
+var message;
+message = playerName + " has health " + health + " and is in " + locationName;
+console.log(message);
 
 console.log(playerName + " is in " + locationName);
 
