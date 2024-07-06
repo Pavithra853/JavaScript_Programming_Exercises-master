f63f2485026f6922a7d7af91360689d80002a6cf
pavithrall <pavithraradhai87@gmail.com>
2024-07-06 19:26:20 +0530

    ch3_pgm_07.js

--- 03_JS_Objects/Ch3_pgm_07.js
+++ 03_JS_Objects/Ch3_pgm_07.js
@@ -1,17 +1,22 @@
 // Objects with multiple properties
-
 var book1;
 var book2;
 
 book1 = {
-    title : "The Hobbit",
-    author : "J. R. R. Tolkien"
+    title  : "The Hobbit",
+    author : "J. R. R. Tolkien",
+    year   : 1937
 };
 
 book2 = {
-	title : "Northern Lights",
-	author : "Philip Pullman"
+    title  : "Northern Lights",
+    author : "Philip Pullman",
+    year   : 1995
 };
+
+console.log(book1);
+console.log(book2);
+
 
 
 
