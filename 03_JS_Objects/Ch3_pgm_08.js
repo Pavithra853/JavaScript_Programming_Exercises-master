a4d831c02bb4a14ab3805ef22588c7f48d87ec97
pavithrall <pavithraradhai87@gmail.com>
2024-07-06 19:39:57 +0530

    ch3_pgm_08.js

--- 03_JS_Objects/Ch3_pgm_08.js
+++ 03_JS_Objects/Ch3_pgm_08.js
@@ -4,13 +4,28 @@ var book;
 
 book = {
   title     : "The Hobbit",
-	author    : "J. R. R. Tolkien",
-	published : 1937
+  author    : "J. R. R. Tolkien",
+  published : 1937
 };
 
-console.log(book.title);
-console.log(book.author);
+console.log(book.title);    // Logs the title property of the book object
+console.log(book.author);   // Logs the author property of the book object
 
+// Further Adventures
+// 1) Log the published property to the console.
+console.log(book.published);    // Logs the published property of the book object
+
+// 2) Add a second book.
+var secondBook = {
+  title     : "Harry Potter and the Philosopher's Stone",
+  author    : "J.K. Rowling",
+  published : 1997
+};
+
+// 3) Log its details to the console.
+console.log(secondBook.title);     // Logs the title of the second book
+console.log(secondBook.author);    // Logs the author of the second book
+console.log(secondBook.published); // Logs the published year of the second book
 
 
 /* Further Adventures
