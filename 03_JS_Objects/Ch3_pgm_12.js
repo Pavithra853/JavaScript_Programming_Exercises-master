bd21053cee3c33f152928223baf8b77285fd5455
pavithrall <pavithraradhai87@gmail.com>
2024-07-09 15:04:06 +0530

    ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -7,6 +7,21 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+// Define a second post
+var secondPost = {
+    id: 2,
+    title: "Journey Through Nebula",
+    author: "Voyager",
+    created: "2018-11-30",
+    body: "Entering the depths of a glowing nebula, where stars are born..."
+};
+
+// Displaying properties of the second post
+console.log("\nSecond Post Title:", secondPost.title);
+console.log("Author:", secondPost.author);
+console.log("Created Date:", secondPost.created);
+console.log("Body:", secondPost.body);
+
 
 
 
