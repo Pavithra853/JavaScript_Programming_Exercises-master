375d2923535d0f7717733cbae033caa20281fb1f
pavithrall <pavithraradhai87@gmail.com>
2024-07-09 15:27:15 +0530

    ch3_pgm_15.js

--- 03_JS_Objects/Ch3_pgm_15.js
+++ 03_JS_Objects/Ch3_pgm_15.js
@@ -14,6 +14,33 @@ console.log(player.name + " is in " + player.place);
 console.log(player.name + " has health " + player.health);
 console.log("Items: " + player.items);
 
+// Define the first player
+var player1 = {
+    name: "Kandra",
+    health: 50,
+    place: "The Dungeon of Doom",
+    items: ["a rusty key", "The Sword of Destiny", "a piece of cheese"]
+};
+
+// Log details of the first player
+console.log(player1.name);
+console.log(player1.name + " is in " + player1.place);
+console.log(player1.name + " has health " + player1.health);
+console.log("Items: " + player1.items.join(", ")); // Use .join(", ") to convert array to string with comma separation
+
+// Create a second player
+var player2 = {
+    name: "Gorth",
+    health: 70,
+    place: "The Cavern of Mystery",
+    items: ["a magic potion", "a shield"]
+};
+
+// Log details of the second player
+console.log("\n" + player2.name);
+console.log(player2.name + " is in " + player2.place);
+console.log(player2.name + " has health " + player2.health);
+console.log("Items: " + player2.items.join(", "));
 
 
 /* Further Adventures
