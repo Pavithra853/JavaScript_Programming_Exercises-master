d3ef2af714f543023ba6967150d37edc0bc6a489
pavithrall <pavithraradhai87@gmail.com>
2024-07-10 14:39:13 +0530

    ch5_pgm_10.js

--- 05_JS_Arguments/Ch5_pgm_10.js
+++ 05_JS_Arguments/Ch5_pgm_10.js
@@ -1,27 +1,30 @@
 // Displaying a player's health via object properties
-
 var player1;
 var player2;
 var showPlayerHealth;
 
-showPlayerHealth = function (playerName, playerHealth) {
-    console.log(playerName + " has health " + playerHealth);
+showPlayerHealth = function (playerName, playerHealth, playerHealthMultiplier) {
+    var modifiedHealth = playerHealth * playerHealthMultiplier; // Step 3: Multiply health by healthMultiplier
+    console.log(playerName + " has health " + modifiedHealth);
 };
 
 player1 = {
     name: "Kandra",
     place: "The Dungeon of Doom",
-    health: 50
+    health: 50,
+    healthMultiplier: 1.5  // Step 1: Add healthMultiplier property
 };
 
 player2 = {
     name: "Dax",
     place: "The Old Library",
-    health: 40
+    health: 40,
+    healthMultiplier: 2  // Step 1: Add healthMultiplier property
 };
 
-showPlayerHealth(player1.name, player1.health);
-showPlayerHealth(player2.name, player2.health);
+showPlayerHealth(player1.name, player1.health, player1.healthMultiplier);  // Step 4: Pass healthMultiplier as third argument
+showPlayerHealth(player2.name, player2.health, player2.healthMultiplier);  // Step 4: Pass healthMultiplier as third argument
+
 
 
 
