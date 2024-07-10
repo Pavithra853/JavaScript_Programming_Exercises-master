528c60e25c824dbee857233481ef729e51319959
pavithrall <pavithraradhai87@gmail.com>
2024-07-10 14:14:41 +0530

    ch8_pgm_08.js

--- 05_JS_Arguments/Ch5_pgm_08.js
+++ 05_JS_Arguments/Ch5_pgm_08.js
@@ -23,6 +23,28 @@ player2 = {
 showPlayerName(player1.name);
 showPlayerName(player2.name);
 
+var player1;
+var player2;
+var showPlayerName;
+
+showPlayerName = function (playerName) {
+    console.log(playerName.toLowerCase());
+};
+
+player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
+
+player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+showPlayerName(player1.name); // Outputs: kandra
+showPlayerName(player2.name); // Outputs: dax
 
 
 /* Further Adventures
