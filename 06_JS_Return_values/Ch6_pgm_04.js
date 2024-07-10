eaf97481040fe3e588082d13dd1e1f27f552ee1b
pavithrall <pavithraradhai87@gmail.com>
2024-07-10 15:34:39 +0530

    ch6_pgm_04.js

--- 06_JS_Return_values/Ch6_pgm_04.js
+++ 06_JS_Return_values/Ch6_pgm_04.js
@@ -1,16 +1,34 @@
 // Returning the sum of two numbers
 
 var add;
+var addThreeNumbers;
 
 add = function (number1, number2) {
-	var total = number1 + number2;
-
-	return total;
+    var total = number1 + number2;
+    return total;
 };
 
-var sum = add(50, 23);
+addThreeNumbers = function (number1, number2, number3) {
+    var total = number1 + number2 + number3;
+    return total;
+};
 
-console.log(sum);
+// Task 1: Sum of a different pair of numbers
+var newSum = add(10, 15);
+console.log(newSum);
+
+// Task 2: Displaying the sum with a message
+var sum = add(50, 23);
+console.log("The sum of 50 and 23 is " + sum);
+
+// Task 3: Adding more than two numbers using nested calls
+var sum1 = add(10, 15);
+var sum2 = add(sum1, 20);
+console.log("The sum of 10, 15, and 20 is " + sum2);
+
+// Task 4: Sum of three numbers
+var sumOfThree = addThreeNumbers(10, 20, 30);
+console.log("The sum of 10, 20, and 30 is " + sumOfThree);
 
 
 
