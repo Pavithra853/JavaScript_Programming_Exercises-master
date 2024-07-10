dbcc7cf4c51d292b7972df1fa030745a0c9d9ee8
pavithrall <pavithraradhai87@gmail.com>
2024-07-10 15:22:12 +0530

    ch6-pgm_01.js

--- 06_JS_Return_values/Ch6_pgm_01.js
+++ 06_JS_Return_values/Ch6_pgm_01.js
@@ -1,17 +1,23 @@
 // Returning a value from a function
 
 var getMessage;
+var getMyMessage;
 var response;
 
 getMessage = function () {
     return "I’m going on an adventure!";
 };
 
+getMyMessage = function () {
+    return "This is my custom message!";
+}
+
 response = getMessage();
 
 console.log(response);
 
-
+response = getMyMessage();
+console.log(response);
 
 /* Further Adventures
  *
