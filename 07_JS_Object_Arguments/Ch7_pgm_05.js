a95d8a6bd92c7921a062bd6f1cc73b938baa5485
pavithrall <pavithraradhai87@gmail.com>
2024-07-11 15:10:43 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -10,7 +10,24 @@ var showSmaller = function (num1, num2) {
 showSmaller(12, 3);
 showSmaller(-10, 3);
 
+var showLarger = function (num1, num2) {
+    var smaller = Math.min(num1, num2);
+    var larger = Math.max(num1, num2);
 
+    console.log(larger + " is larger than " + smaller);
+};
+
+// Example use of showLarger function
+showLarger(12, 3);   // Output: 12 is larger than 3
+
+var showSmallest = function (num1, num2, num3) {
+    var smallest = Math.min(num1, num2, num3);
+
+    console.log(smallest + " is the smallest among " + num1 + ", " + num2 + ", and " + num3);
+};
+
+// Example use of showSmallest function
+showSmallest(5, 10, 3);   // Output: 3 is the smallest among 5, 10, and 3
 
 /* Further Adventures
  *
