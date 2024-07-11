e143bb0fde588b7a911d6ebfcdd962d519b27201
pavithrall <pavithraradhai87@gmail.com>
2024-07-11 14:59:51 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_01.js
+++ 07_JS_Object_Arguments/Ch7_pgm_01.js
@@ -1,6 +1,6 @@
 // Passing a function an object as an argument
 
-var planet1;
+var planet1, planet2;
 var getPlanetInfo;
 
 planet1 = {
@@ -11,11 +11,28 @@ planet1 = {
     sizeRank: 1
 };
 
-getPlanetInfo = function (planet) {
-    return planet.name + ": planet number " + planet.position;
+// Creating a second planet object
+planet2 = {
+    name: "Saturn",
+    position: 6,
+    type: "Gas Giant",
+    radius: 58232,
+    sizeRank: 2
 };
 
+// Updated getPlanetInfo function to include more information
+getPlanetInfo = function (planet) {
+    return planet.name + ": planet number " + planet.position +
+           "\nType: " + planet.type +
+           "\nRadius: " + planet.radius + " km" +
+           "\nSize Rank: " + planet.sizeRank + "\n";
+};
+
+// Log details of the first planet
 console.log(getPlanetInfo(planet1));
+
+// Log details of the second planet
+console.log(getPlanetInfo(planet2));
 
 
 
