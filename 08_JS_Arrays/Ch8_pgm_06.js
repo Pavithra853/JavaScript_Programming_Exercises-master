9276beba44a2df05093accdc349b1edac53b0fe0
pavithrall <pavithraradhai87@gmail.com>
2024-07-12 14:16:41 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_06.js
+++ 08_JS_Arrays/Ch8_pgm_06.js
@@ -15,6 +15,36 @@ removed = items.pop();
 console.log(removed + " was removed");
 console.log(items.join(" and "));
 
+var items = [];
+var item = "The Pyramids";
+var removed;
+
+items.push(item);
+items.push("The Grand Canyon");
+items.push("Bondi Beach");
+
+console.log(items);
+
+removed = items.pop();
+
+console.log(removed + " was removed");
+console.log(items.join(" and "));
+
+// Further Adventures
+
+// 1) Push another item onto the array.
+items.push("Eiffel Tower");
+
+// 2) Log the joined items.
+console.log(items.join(" and "));
+
+// 3) Set one of the items using square brackets.
+items[2] = "The Taj Mahal";
+
+// 4) Can you push more than one item at a time?
+items.push("Statue of Liberty", "Great Wall of China");
+
+console.log(items);
 
 
 /* Further Adventures
