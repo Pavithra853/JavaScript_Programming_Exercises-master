3bb3b429db3e4c793a3fc7b474b680bd030b3497
pavithrall <pavithraradhai87@gmail.com>
2024-07-12 14:50:24 +0530

    completed

--- 09_JS_Constructors/Ch9_pgm_01.js
+++ 09_JS_Constructors/Ch9_pgm_01.js
@@ -20,7 +20,15 @@ console.log(planet1.name);
 console.log(planet1.position);
 console.log(planet1.type);
 
+var planet2 = buildPlanet(
+    "Mars",
+    4,
+    "Terrestrial"
+);
 
+// 2) Log its name and type.
+console.log(planet2.name);
+console.log(planet2.type);
 
 /* Further Adventures
  *
