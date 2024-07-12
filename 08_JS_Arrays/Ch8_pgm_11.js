a40f7bc9738782234c2ecb7740c379855e305edf
pavithrall <pavithraradhai87@gmail.com>
2024-07-12 14:39:06 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_11.js
+++ 08_JS_Arrays/Ch8_pgm_11.js
@@ -15,7 +15,46 @@ var numOfEach = [ 2, 1, 5, 2 ];
 
 console.log("The total cost is $" + getTotalBill(costs, numOfEach));
 
+var items = [
+	{ cost: 1.99, numberBought: 2 },
+	{ cost: 4.95, numberBought: 1 },
+	{ cost: 2.50, numberBought: 5 },
+	{ cost: 9.87, numberBought: 2 },
+	{ cost: 3.75, numberBought: 3 }
+  ];
 
+  var getTotalBill = function (items) {
+	var total = 0;
+  
+	items.forEach(function (item) {
+	  total += item.cost * item.numberBought;
+	});
+  
+	return total;
+  };
+  
+  console.log("The total cost is $" + getTotalBill(items));
+
+  var items = [
+	{ cost: 1.99, numberBought: 2 },
+	{ cost: 4.95, numberBought: 1 },
+	{ cost: 2.50, numberBought: 5 },
+	{ cost: 9.87, numberBought: 2 },
+	{ cost: 3.75, numberBought: 3 }
+  ];
+  
+  var getTotalBill = function (items) {
+	var total = 0;
+  
+	items.forEach(function (item) {
+	  total += item.cost * item.numberBought;
+	});
+  
+	return total;
+  };
+  
+  console.log("The total cost is $" + getTotalBill(items));
+  
 
 /* Further Adventures
  *
