1fdbf6fc0a9effecdd3e622b6d7b29fd5f116660
pavithrall <pavithraradhai87@gmail.com>
2024-07-12 14:43:19 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_12.js
+++ 08_JS_Arrays/Ch8_pgm_12.js
@@ -25,7 +25,81 @@ var displayQuestion = function (questionAndAnswer) {
   
   displayQuestion(question1);
   
+  var question2 = {
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      "Earth",
+      "Jupiter",
+      "Mars",
+      "Venus"
+    ],
+    correctAnswer: "Jupiter"
+  };
   
+  var question3 = {
+    question: "What is the smallest unit of life?",
+    answers: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organ"
+    ],
+    correctAnswer: "Cell"
+  };
+
+  var questions = [question1, question2, question3];
+
+  questions.forEach(displayQuestion);
+
+  var displayQuestion = function (questionAndAnswer) {
+    var options = ["A", "B", "C", "D", "E"];
+    
+    console.log(questionAndAnswer.question);
+    
+    questionAndAnswer.answers.forEach(function (answer, i) {
+      console.log(options[i] + " - " + answer);
+    });
+  };
+  
+  var question1 = {
+    question: "What is the capital of France?",
+    answers: [
+      "Bordeaux",
+      "F",
+      "Paris",
+      "Brussels"
+    ],
+    correctAnswer: "Paris"
+  };
+  
+  var question2 = {
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      "Earth",
+      "Jupiter",
+      "Mars",
+      "Venus"
+    ],
+    correctAnswer: "Jupiter"
+  };
+  
+  var question3 = {
+    question: "What is the smallest unit of life?",
+    answers: [
+      "Atom",
+      "Molecule",
+      "Cell",
+      "Organ"
+    ],
+    correctAnswer: "Cell"
+  };
+  
+  var questions = [question1, question2, question3];
+  
+  questions.forEach(displayQuestion);
+  
+  // To display a particular question, you can call:
+  displayQuestion(question2);
   
   /* Further Adventures
    *
