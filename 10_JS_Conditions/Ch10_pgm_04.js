94ffe7561e133e58109bfda1c713766270a57030
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 16:14:38 +0530

    completed

--- 10_JS_Conditions/Ch10_pgm_04.js
+++ 10_JS_Conditions/Ch10_pgm_04.js
@@ -14,8 +14,27 @@ var getGuesser = function () {
   
   var guess = getGuesser();
   
+  var between = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var getGuesser = function () {
+    var secret = between(30, 50);
   
-  
+    return function (userNumber) {
+      if (userNumber === secret) {
+        return "Well done!";
+      } else {
+        return "Unlucky, try again.";
+      }
+    };
+};
+
+var guess = getGuesser();
+
+console.log(guess(35));  
+console.log(guess(42)); 
+
   /* Further Adventures
    *
    * 1) Run the program.
