a0aa3a9dad4e8f99103946d1f5eb2c60b65a85e6
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 18:01:48 +0530

    completed

--- 11_JS_Promises/Ch11_pgm_02.js
+++ 11_JS_Promises/Ch11_pgm_02.js
@@ -3,19 +3,15 @@
 // In this case the err is false and it will return the else block which is the result.
 
 const doSomething = callback => {
-    setTimeout(() => {
+  setTimeout(() => {
       const skills = ['HTML', 'CSS', 'JS']
       callback(false, skills)
-    }, 2000)
-  }
-  
-  doSomething((err, result) => {
-    if (err) {
+  }, 2000)
+}
+
+doSomething((err, result) => {
+  if (err) {
       return console.log(err)
-    }
-    return console.log(result)
-  })
-
-
-// after 2 seconds it will print the skills
-// ["HTML", "CSS", "JS"]
+  }
+  return console.log(result)
+})
