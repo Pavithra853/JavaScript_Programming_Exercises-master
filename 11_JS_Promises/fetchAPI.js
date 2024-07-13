481ec49102afd0724b3c7f74e2f1d3a17610a894
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 18:18:02 +0530

    completed

--- 11_JS_Promises/fetchAPI.js
+++ 11_JS_Promises/fetchAPI.js
@@ -1,14 +1,9 @@
-// The Fetch API provides an interface for fetching resources (including across the network). 
-// It will seem familiar to anyone who has used XMLHttpRequest, but the new API provides a more 
-// powerful and flexible feature set. In this challenge we will use fetch to request url and APIS. 
-// In addition to that let us see demonstrate use case of promises in accessing network resources 
-// using the fetch API.
+const url = 'https://restcountries.com/v2/all'; // countries API URL
 
-const url = 'https://restcountries.com/v2/all' // countries api
-fetch(url)
-  .then(response => response.json()) // accessing the API data as JSON
+fetch(url) // Fetch data from the URL
+  .then(response => response.json()) // Convert response to JSON format
   .then(data => {
-    // getting the data
-    console.log(data)
+    // Handle the JSON data
+    console.log(data); // Print the data to the console (or do further processing)
   })
-  .catch(error => console.error(error)) // handling error if something wrong happens
+  .catch(error => console.error(error)); // Catch and handle any errors that occur
