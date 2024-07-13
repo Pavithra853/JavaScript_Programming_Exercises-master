fb693ce49fb7fedb970f58931b33767adef57b30
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 10:52:19 +0530

    completed

--- 10_JS_Conditions/Ch10_pgm_01.js
+++ 10_JS_Conditions/Ch10_pgm_01.js
@@ -8,8 +8,16 @@ var guess = function (userNumber) {
   }
 };
 
+var secret = 8;
 
+var guess = function (userNumber) {
+  if (userNumber > secret) {
+    console.log("Too high!");
+  }
+};
 
+guess(9);
+guess(7);
 /* Further Adventures
  *
  * 1) Click run.
