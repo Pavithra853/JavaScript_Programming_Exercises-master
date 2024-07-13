cf87d438737f62dd59c596b369e5ab2af4565ac8
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 18:10:49 +0530

    completed

--- 11_JS_Promises/Ch11_pgm_06.js
+++ 11_JS_Promises/Ch11_pgm_06.js
@@ -2,3 +2,19 @@ const countriesAPI = 'https://restcountries.com/v2/all'
 const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 
 // Print out all the cat names in to catNames variable.
+
+
+fetch(catsAPI)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const catNames = data.map(cat => cat.name);
+    console.log(catNames);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
