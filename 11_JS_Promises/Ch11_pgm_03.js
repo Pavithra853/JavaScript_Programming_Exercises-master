5b6a25c110d998fbebd5257bab8a46c30ce0f1bc
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 18:04:50 +0530

    completed

--- 11_JS_Promises/Ch11_pgm_03.js
+++ 11_JS_Promises/Ch11_pgm_03.js
@@ -1,30 +1,17 @@
-// Promise constructor
-
-// syntax
-
-// const promise = new Promise((resolve, reject) => {
-//     resolve('success')
-//     reject('failure')
-//   })
-
 // Promise
 const doPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+  setTimeout(() => {
       const skills = ['HTML', 'CSS', 'JS']
       if (skills.length > 0) {
-        resolve(skills)
+          resolve(skills)
       } else {
-        reject('Something wrong has happened')
+          reject('Something wrong has happened')
       }
-    }, 2000)
-  })
-  
-  doPromise
-    .then(result => {
+  }, 2000)
+})
+
+doPromise
+  .then(result => {
       console.log(result)
-    })
-    .catch(error => console.log(error))
-
-
-
-    // ["HTML", "CSS", "JS"]
+  })
+  .catch(error => console.log(error))
