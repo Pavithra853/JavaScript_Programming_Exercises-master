72fea2f5fbb12fa01cfb7d1da520f363ba6042fd
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 18:06:26 +0530

    completed

--- 11_JS_Promises/Ch11_pgm_04.js
+++ 11_JS_Promises/Ch11_pgm_04.js
@@ -1,22 +1,23 @@
 // The Ch11_pgm_03 promise has been settled with resolve. Let us another example when the promise is settled with reject.
 
 // Promise
+// Promise
 const doPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+  setTimeout(() => {
       const skills = ['HTML', 'CSS', 'JS']
       if (skills.includes('Node')) {
-        resolve('fullstack developer')
+          resolve('fullstack developer')
       } else {
-        reject('Something wrong has happened')
+          reject('Something wrong has happened')
       }
-    }, 2000)
-  })
-  
-  doPromise
-    .then(result => {
+  }, 2000)
+})
+
+doPromise
+  .then(result => {
       console.log(result)
-    })
-    .catch(error => console.error(error))
+  })
+  .catch(error => console.error(error))
 
 
 
