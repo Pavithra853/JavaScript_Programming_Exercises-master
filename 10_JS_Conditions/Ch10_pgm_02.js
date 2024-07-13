26d0d98ec49b8d537dad245edbcdf5618d69a5a4
pavithrall <pavithraradhai87@gmail.com>
2024-07-13 10:55:57 +0530

    completed

--- 10_JS_Conditions/Ch10_pgm_02.js
+++ 10_JS_Conditions/Ch10_pgm_02.js
@@ -10,6 +10,37 @@ var guess = function (userNumber) {
   }
 };
 
+var secret = 8;
+
+var guess = function (userNumber) {
+  if (userNumber !== secret) {
+    console.log("Unlucky, try again.");
+  } else {
+    console.log("Well done!");
+  }
+};
+
+guess(2); 
+guess(8); 
+
+/* Further Adventures
+ *
+ * 1) Run the program and try a few guesses at the prompt.
+ *    e.g. guess(2)
+ *
+ * You can check if two values are NOT equal
+ * by using the !== operator.
+ *
+ * 2) Change the condition in parentheses to check
+ *    if the guess is not equal to the secret.
+ *
+ *    if (userNumber !== secret)
+ *
+ * 3) Run and test the updated program.
+ *
+ * 4) Change the messages to fit the new condition.
+ *
+ */
 
 
 /* Further Adventures
